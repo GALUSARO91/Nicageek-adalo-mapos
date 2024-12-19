11ed5a2fb82cc0a1701b17b50877a4668ec50275
galusarom <glsr91@hotmail.com>
2024-12-18 20:26:57 -0600

    first remote commit

---
+++ src/components/CustomStaticMap/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {View, StyleSheet, Image} from 'react-native'
+
+
+
+const CustomMaps = (props) => {
+	const {lat,long,hgt,wdt,zoom,lang,theme} = props
+
+	const mapUri = `https://static-maps.yandex.ru/1.x/?lang=${lang}&ll=${long},${lat}&z=${zoom}&l=map&size=${wdt},${hgt}&pt=${long},${lat},pm2rdm`;
+
+	return(
+		<View style={styles.wrapper}>
+			<Image
+				style={[styles.customMap, {height:hgt,width:wdt}]}
+				source={mapUri}
+			/>
+			
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	wrapper: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	customMap:{
+		borderColor:'#000',
+		borderStyle: 'solid',
+		borderWidth: 1,
+		resizeMode: 'contain',
+		objectFit:'contain',
+		overflow:'hidden'
+	}
+
+})
+
+export default CustomMaps
